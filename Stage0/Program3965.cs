1c972c55747ea879274046f3daf08837d2f0da92
Miri Cheshin <mirishin7590@gmail.com>
2025-10-20 21:52:42 +0300

    Add project files.

---
+++ Stage0/Program3965.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Win32.SafeHandles;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        Welcome3965();
+        Welcome0559();
+        Console.ReadKey();
+        string s;
+
+    }
+
+    static partial void Welcome0559();
+
+    private static void Welcome3965()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0} , Welcome to my first console application", name);
+    }
+}
