b233dd0a548d691229bb89f046212b71e168563d
hadas wert <hadaswert1403@gmail.com>
2025-10-29 14:20:00 +0200

    Enums fixings

--- DalFacade/DO/Courier.cs
+++ DalFacade/DO/Courier.cs
@@ -1,6 +1,6 @@
 ﻿/// Module Courier.cs
 
-namespace DO; 
+namespace DO;
 /// <summary>
 /// Courier Entity represents a courier with all its props
 /// </summary>
@@ -10,7 +10,7 @@ namespace DO;
 /// <param name="Email">The email address of the courier</param>
 /// <param name="Password">The private password of the courier</param>
 /// <param name="Active">whether the courier is active (default true)</param>
-/// <param name="CourierType">Delivery type: car, motorcycle, bicycle, or on foot</param>
+/// <param name="DeliveryMethod">Delivery method: car, motorcycle, bicycle, or on foot</param>
 /// <param name="StartedWorking">The time when the courier started to work</param>
 /// <param name="MaxPersonalDistance">Max aerial distance from company to order address the courier agrees to deliver</param>
 public record Courier
@@ -21,7 +21,7 @@ public record Courier
     string Email,
     string Password,
     bool Active,
-    EnumCourierType CourierType,
+    EnumDeliveryMethod DeliveryMethod,
     DateTime StartedWorking,
     double? MaxPersonalDistance = 0
 )
@@ -29,6 +29,6 @@ public record Courier
     /// <summary>
     /// Default constructor for stage 3
     /// </summary>
-    public Courier() : this(0, "", "", "", "", true, EnumCourierType.Car, DateTime.Now) {}
+    public Courier() : this(0, "", "", "", "", true, EnumDeliveryMethod.Car, DateTime.Now) {}
 }
 
--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -3,17 +3,26 @@
 /// <summary>
 /// Used in Courier entity: eumCourierType enum represents the type of courier.
 /// </summary>
-public enum EnumCourierType
+public enum EnumDeliveryMethod
 {
     Car, Motorcycle, Bicycle, Foot
 };
 
 
 /// <summary>
-/// Used in Order entity: eumOrderType enum represents the type of an order. 
+/// Used in Order entity: EumOrderType represents the type of an order. 
 /// </summary>
-public enum EnumOrderType 
+public enum EnumOrderType
 {
     Regular, Express, Overnight
 };
 
+/// <summary>
+/// Used in Delivery entity: EnumEndDeliveryStatus represents the final status of a delivery process.
+/// </summary>
+/// <remarks>This enumeration provides various outcomes for a delivery attempt, indicating whether it was
+/// successful, refused, canceled, or failed due to other reasons.</remarks>
+public enum EnumEndDeliveryStatus
+{
+    Delivered, RefusedToReceive, Canceled, CustomerNotFound, Failed
+}
