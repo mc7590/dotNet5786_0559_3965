7c14f4a4f62043195d2498b1b49b32b5cf38c221
Miri Cheshin <mirishin7590@gmail.com>
2025-10-29 14:23:49 +0200

    create DalList -> Config.cs

---
+++ DalList/Config.cs
@@ -0,0 +1,26 @@
+﻿namespace Dal;
+
+static internal class Config
+{
+    internal static int NextOrderId = 1000;
+    internal static int NextDeliveryId = 1000;
+    internal static DateTime Clock { get; set; } = DateTime.Now;
+    internal static int ManagerId;
+    internal static string ManagerPassword = "BDI846924?/";
+    internal const string? CompanyAddress = null;
+    internal static double? Latitude = null;
+    internal static double? Longitude = null;
+    internal static double? MaxDeliveryDistance = null;
+    internal static double AveCarSpeedKmH;
+    internal static double AveMotorcycleSpeedKmH;
+    internal static double AveBicycleSpeedKmH;
+    internal static double AveWalkingSpeedKmH;
+    internal static TimeSpan GetMaxDeliveryTime;
+    internal static TimeSpan RiskRange;
+    internal static TimeSpan InactivityThreshold;
+
+    internal static void Reset()
+    {
+        Clock = DateTime.Now;
+    }
+}
