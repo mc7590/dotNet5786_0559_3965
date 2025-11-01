e865d8089f755ba5b239cbaaa2b3d989fb5ea34b
hadas wert <hadaswert1403@gmail.com>
2025-11-01 23:06:17 +0200

    create: DalTest->Initialization.cs

---
+++ DalTest/Initialization.cs
@@ -0,0 +1,17 @@
+﻿//Initialization of DAL tests
+namespace DalTest;
+using DalApi;
+using DO;
+
+public static class Initialization
+{
+    private static ICourier? s_dalCourier; //stage 1
+    private static IOrder? s_dalOrder; //stage 1
+    private static IDelivery? s_dalDelivery; //stage 1
+    private static IConfig? s_dalConfig; //stage 1
+
+    /// <summary>
+    /// Random number generator to initialize test data
+    /// </summary>
+    private static readonly Random s_rand = new();
+}
