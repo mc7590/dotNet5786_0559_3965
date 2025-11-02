e27543ee798f6995ccaeefecb2afa12c4b84c6c1
hadas wert <hadaswert1403@gmail.com>
2025-11-02 21:55:23 +0200

    add emuns: DalFacade->DO->Enums

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -40,3 +40,87 @@ public enum MainMenuOption
     InitializeData = 5,
     ResetAll = 6
 }
+
+/// <summary>
+/// Enum for courier menu
+/// </summary>
+public enum CourierMenuOption
+{
+    Exit = 0,
+    AddCourier = 1,
+    UpdateCourier = 2,
+    DeleteCourier = 3,
+    GetCourier = 4,
+    GetAllCouriers = 5
+}
+
+/// <summary>
+/// Enum for order menu
+/// </summary>
+public enum OrderMenuOption
+{
+    Exit = 0,
+    AddOrder = 1,
+    UpdateOrder = 2,
+    DeleteOrder = 3,
+    GetOrder = 4,
+    GetAllOrders = 5
+}
+
+/// <summary>
+/// Enum for delivery menu
+/// </summary>
+public enum DeliveryMenuOption
+{
+    Exit = 0,
+    AddDelivery = 1,
+    UpdateDelivery = 2,
+    DeleteDelivery = 3,
+    GetDelivery = 4,
+    GetAllDeliveries = 5
+}
+
+/// <summary>
+/// Enum for config menu
+/// </summary>
+public enum ConfigMenuOption
+{
+    Exit=0,
+    Add1MinToClock=1,
+    Add1HourToClock=2,
+    Add1DayToClock=3,
+    ShowCurrentClock= 4,
+    SetConfigParameters=5,
+    GetConfigParameters=6,
+    ResetConfigToDefault= 7
+}
+
+/// <summary>
+/// Enum for set config options
+/// </summary>
+public enum SetConfigParametersOption
+{
+    Back = 0,
+    SetClock=1,
+    SetCompanyAddress=2,
+    SetLatitude=3,
+    SetLongitude=4,
+    SetMaxDeliveryDistance=5,
+}
+
+/// <summary>
+/// Enum for get config options
+/// </summary>
+public enum GetConfigParametersOption
+{
+    Back = 0,
+    GetClock = 1,
+    GetCompanyAddress = 2,
+    GetLatitude = 3,
+    GetLongitude = 4,
+    GetMaxDeliveryDistance = 5,
+    GetMaxDeliveryTime = 6,
+    GetRiskRange = 7,
+    GetInactivityThreshold = 8
+}
+
