fd3874065e9470f165f38b414bbc33df85840394
hadas wert <hadaswert1403@gmail.com>
2025-11-02 19:23:31 +0200

    create main: DalTest->program

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace DalTest
+﻿using Dal;
+using DalApi;
+
+namespace DalTest;
+
+
+internal class Program
 {
-    internal class Program
+    private static ICourier s_dalCourier = new CourierImplementation(); //stage 1
+    private static IOrder? s_dalOrder = new OrderImplementation(); //stage 1
+    private static IDelivery? s_dalDelivery = new DeliveryImplementation(); //stage 1
+    private static IConfig? s_dalConfig = new ConfigImplementation(); //stage 1
+    
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        try
         {
-            Console.WriteLine("Hello, World!");
+           //main...
+
+
+
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
         }
     }
+    
+
+    
 }
