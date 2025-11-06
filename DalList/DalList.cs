f52247c35a759ef900288a424e4b3c1ee055bfc9
Miri Cheshin <mirishin7590@gmail.com>
2025-11-06 22:31:53 +0200

    Create DalFacade -> DalApi -> IDal + DalList -> DalList.cs

---
+++ DalList/DalList.cs
@@ -0,0 +1,25 @@
+﻿namespace Dal;
+using DalApi;
+
+sealed public class DalList : IDal
+{
+    public ICourier Courier { get; } = new CourierImplementation();
+
+    public IOrder Order { get; } = new OrderImplementation();
+
+    public IDelivery Delivery { get; } = new DeliveryImplementation();
+
+    public IConfig Config { get; } = new ConfigImplementation();
+
+    public void ResetDB()
+    {
+        Console.WriteLine("Deleting Couriers...");
+        Courier.DeleteAll(); 
+        Console.WriteLine("Deleting Orders...");
+        Order.DeleteAll();
+        Console.WriteLine("Deleting Deliveries...");
+        Delivery.DeleteAll();
+        Console.WriteLine("Resetting Config...");
+        Config.Reset();
+    }
+}
