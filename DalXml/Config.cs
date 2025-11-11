e77868914be7746269d578155d49b4f2f77e2f6d
Miri Cheshin <mirishin7590@gmail.com>
2025-11-11 23:06:52 +0200

    Create DalXml -> Config

---
+++ DalXml/Config.cs
@@ -0,0 +1,36 @@
+﻿namespace Dal;
+
+internal static class Config
+{
+    internal const string s_data_config_xml = "data-config.xml";
+    internal const string s_couriers_xml = "couriers.xml";
+    internal const string s_orders_xml = "orders.xml";
+    internal const string s_deliverys_xml = "deliverys.xml";
+
+    internal static int NextOrderId
+    {
+        get => XMLTools.GetAndIncreaseConfigIntVal(s_data_config_xml, "NextOrderId");
+        private set => XMLTools.SetConfigIntVal(s_data_config_xml, "NextOrderId", value);
+    }
+    internal static int NextDeliveryId
+    {
+        get => XMLTools.GetAndIncreaseConfigIntVal(s_data_config_xml, "NextDeliveryId");
+        private set => XMLTools.SetConfigIntVal(s_data_config_xml, "NextDeliveryId", value);
+    }
+
+
+    internal static DateTime Clock
+    {
+        get => XMLTools.GetConfigDateVal(s_data_config_xml, "Clock");
+        set => XMLTools.SetConfigDateVal(s_data_config_xml, "Clock", value);
+    }
+
+    internal static void Reset()
+    {
+        NextOrderId = 1000;
+        NextDeliveryId = 1000;
+         
+        Clock = DateTime.Now;
+    }
+
+}
