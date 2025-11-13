813faf0c36defac57e9590f66ce03d15ac613131
hadas wert <hadaswert1403@gmail.com>
2025-11-13 15:13:26 +0200

    stage3 chapter 6

---
+++ DalXml/ConfigImplementation.cs
@@ -0,0 +1,90 @@
+﻿//ConfigImplementation.cs
+using DalApi;
+
+namespace Dal;
+
+/// <summary>
+/// Configuration implementation class.
+/// </summary>
+internal class ConfigImplementation : IConfig
+{
+    /// <summary>
+    /// Get/set the current clock time used for configuration.
+    /// </summary>
+    public DateTime Clock
+    {
+        get => Config.Clock;
+        set => Config.Clock = value;
+    }
+
+    /// <summary>
+    /// Get/set the company address.
+    /// </summary>
+    public string? CompanyAddress
+    {
+        get => Config.CompanyAddress;
+        set => Config.CompanyAddress = value;
+    }
+
+    /// <summary>
+    /// Get/set the latitude coordinate.
+    /// </summary>
+    public double? Latitude
+    {
+        get => Config.Latitude;
+        set => Config.Latitude = value;
+    }
+
+    /// <summary>
+    /// Get/set the longitude coordinate.
+    /// </summary>
+    public double? Longitude
+    {
+        get => Config.Longitude;
+        set => Config.Longitude = value;
+    }
+
+    /// <summary>
+    /// Get/set the max delivery distance.
+    /// </summary>
+    public double? MaxDeliveryDistance
+    {
+        get => Config.MaxDeliveryDistance;
+        set => Config.MaxDeliveryDistance = value;
+    }
+
+    /// <summary>
+    /// Get/set the max delivery time.
+    /// </summary>
+    public TimeSpan GetMaxDeliveryTime
+    {
+        get => Config.GetMaxDeliveryTime;
+        set => Config.GetMaxDeliveryTime = value;
+    }
+
+    /// <summary>
+    /// Get/set the time range for risk deliveries.
+    /// </summary>
+    public TimeSpan RiskRange
+    {
+        get => Config.RiskRange;
+        set => Config.RiskRange = value;
+    }
+
+    /// <summary>
+    /// Get/set the max inactivity time for couriers.
+    /// </summary>
+    public TimeSpan InactivityThreshold
+    {
+        get => Config.InactivityThreshold;
+        set => Config.InactivityThreshold = value;
+    }
+
+    /// <summary>
+    /// Resets all configuration data to the initial values.
+    /// </summary>
+    public void Reset()
+    {
+        Config.Reset();
+    }
+}
---
+++ DalXml/OrderImplementation.cs
@@ -0,0 +1,59 @@
+﻿
+namespace Dal;
+using DalApi;
+using DO;
+using System;
+using System.Collections.Generic;
+
+internal class OrderImplementation : IOrder
+{
+    //working by 1st method
+
+    public void Create(Order item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        List<Order> Orders = XMLTools.LoadListFromXMLSerializer<Order>(Config.s_orders_xml);
+        if (Orders.RemoveAll(it => it.Id == id) == 0)
+            throw new DalDoesNotExistException($"Order with ID={id} does Not exist");
+        XMLTools.SaveListToXMLSerializer(Orders, Config.s_orders_xml);
+    }
+
+
+    public void DeleteAll()
+    {
+        XMLTools.SaveListToXMLSerializer(new List<Order>(), Config.s_orders_xml);
+    }
+
+
+    public Order? Read(int id)
+    {
+        List<Order> Orders = XMLTools.LoadListFromXMLSerializer<Order>(Config.s_orders_xml);
+        return Orders.FirstOrDefault(it => it.Id == id);
+    }
+
+    public Order? Read(Func<Order, bool> filter)
+    {
+        List<Order> Orders = XMLTools.LoadListFromXMLSerializer<Order>(Config.s_orders_xml);
+        return Orders.FirstOrDefault(item => filter(item));
+
+    }
+
+    public IEnumerable<Order> ReadAll(Func<Order, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Order item)
+    {
+        List<Order> Orders = XMLTools.LoadListFromXMLSerializer<Order>(Config.s_orders_xml);
+        if (Orders.RemoveAll(it => it.Id == item.Id) == 0)
+            throw new DalDoesNotExistException($"Order with ID={item.Id} does Not exist");
+        Orders.Add(item);
+        XMLTools.SaveListToXMLSerializer(Orders, Config.s_orders_xml);
+    }
+
+}
