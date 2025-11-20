370332434445e4f7c6fb80418304b0099a7778b7
hadas wert <hadaswert1403@gmail.com>
2025-11-20 12:29:04 +0200

    create BO. Order, BO.DeliveryPerOrderInList

---
+++ BL/BO/DeliveryPerOrderInList.cs
@@ -0,0 +1,15 @@
+﻿namespace BO
+{
+    public class DeliveryPerOrderInList
+    {
+        public int DeliveryId { get; init; }
+        public int CourierId { get; init; }
+        public string? CourierName { get; init; }
+        public EnumDeliveryMethod DeliveryMethod { get; init; }
+        public DateTime DelCreationTime { get; init; }
+        public EnumEndDeliveryStatus? EndDeliveryStatus { get; init; }
+        public DateTime? EndDeliveryTime { get; init; }
+
+
+    }
+}
--- BL/BO/Order.cs
+++ BL/BO/Order.cs
@@ -2,4 +2,25 @@
 
 public class Order
 {
+    public int Id { get; init; }
+    public EnumOrderType OrderType { get; set; }
+    public string? Description { get; set; }
+    public string? Address { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double AerialDistance { get; set; }
+    public string? CustomerName { get; set; }
+    public string? CustomerPhone { get; set; }
+    public double? Weight { get; set; }
+    public bool? Fragile { get; set; }
+    public DateTime CreationTime { get; init; }
+    public DateTime? ExpectedDeliveryTime { get; init; }
+    public DateTime? MaxDeliveryTime { get; init; }
+    public EnumOrderStatus OrderStatus { get; init; }
+    public EnumScheduleStatus ScheduleStatus { get; init; }
+    public TimeSpan RemainingTime { get; init; }
+    public List<DeliveryPerOrderInList>? OrderDelivHist { get; init; }
+
+
+
 }
