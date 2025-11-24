7221ffea07f3070bd1cab5ba4076130825ea5423
Miri Cheshin <mirishin7590@gmail.com>
2025-11-24 21:23:25 +0200

    Add functions: BLApi -> ICourier

--- BL/BlApi/ICourier.cs
+++ BL/BlApi/ICourier.cs
@@ -3,4 +3,11 @@ namespace BlApi;
 
 public interface ICourier
 {
+    void Create(BO.Courier boStudent);
+    BO.Courier? Read(int id);
+
+    IEnumerable<BO.CourierInList> ReadAll(BO.CourierFieldSort? sort = null, BO.StudentFieldFilter? filter = null, object? value = null);
+    void Update(BO.Courier boStudent);
+    void Delete(int id);
+
 }
