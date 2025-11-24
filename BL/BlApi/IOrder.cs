88e27f69053daa63368aaf2797c1384dd5c4dd44
hadas wert <hadaswert1403@gmail.com>
2025-11-24 21:22:42 +0200

    add functions: BlApi->IOrder

--- BL/BlApi/IOrder.cs
+++ BL/BlApi/IOrder.cs
@@ -3,4 +3,11 @@ namespace BlApi;
 
 public interface IOrder
 {
+    void Create(BO.Order boOrder);
+    BO.Order? Read(int id);
+
+    IEnumerable<BO.OrderInList> ReadAll(BO.OrderFieldSort? sort = null, BO.OrderFieldFilter? filter = null, object? value = null);
+    void Update(BO.Order boOrder);
+    void Delete(int id);
+
 }
