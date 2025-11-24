6c738051311e309269af5928255c145cada53f10
hadas wert <hadaswert1403@gmail.com>
2025-11-24 21:05:32 +0200

    create interfaces: BL->BlApi->ICourier, IOrder, IAdmin

---
+++ BL/BlApi/IAdmin.cs
@@ -0,0 +1,13 @@
+﻿
+namespace BlApi;
+
+public interface IAdmin
+{
+    void ResetDB();
+    void InitializeDB();
+    DateTime GetClock();
+    void ForwardClock(BO.TimeUnit unit);
+    BO.Config GetConfig();
+    void SetConfig(BO.Config config);
+
+}
---
+++ BL/BlApi/ICourier.cs
@@ -0,0 +1,6 @@
+﻿
+namespace BlApi;
+
+public interface ICourier
+{
+}
