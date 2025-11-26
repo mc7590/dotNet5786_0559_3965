700ef652bf7a8272e4dc339bdf09de72ec9dde32
hadas wert <hadaswert1403@gmail.com>
2025-11-26 20:20:45 +0200

    finish edit BlApi-> IAdmin, BlImplementation->AdminImplementation

--- BL/BlApi/IAdmin.cs
+++ BL/BlApi/IAdmin.cs
@@ -1,13 +1,39 @@
 ﻿
+using Helpers;
+
 namespace BlApi;
 
 public interface IAdmin
 {
+
+    /// <summary>
+    /// Reset all configuration data to its initial value
+    /// </summary>
     void ResetDB();
+
+    /// <summary>
+    /// Initialize database: reset, then initialize
+    /// </summary>
     void InitializeDB();
+
+    /// <summary>
+    /// Returns the system clock
+    /// </summary>
     DateTime GetClock();
-    void ForwardClock(BO.TimeUnit unit);
+
+    /// <summary>
+    /// Advance the system clock by the appropriate time unit (minute, hour, day, month, year)
+    /// </summary>
+    void ForwardClock(BO.EnumTimeUnit unit);
+
+    /// <summary>
+    /// Returns the values ​​of BO.Config
+    /// </summary>
     BO.Config GetConfig();
+
+    /// <summary>
+    /// Updates all configuration variables
+    /// </summary>
     void SetConfig(BO.Config config);
 
 }
--- BL/BlImplementation/AdminImplementation.cs
+++ BL/BlImplementation/AdminImplementation.cs
@@ -1,7 +1,42 @@
 ﻿using BlApi;
+using BO;
+using Helpers;
+
 namespace BlImplementation;
 
 internal class AdminImplementation : IAdmin
 {
+    public void ForwardClock(EnumTimeUnit unit)
+    {
+       switch (unit)
+        {
+            case EnumTimeUnit.Minute:
+                AdminManager.UpdateClock(AdminManager.Now.AddMinutes(1));
+                break;
+            case EnumTimeUnit.Hour:
+                AdminManager.UpdateClock(AdminManager.Now.AddHours(1));
+                break;
+            case EnumTimeUnit.Day:
+                AdminManager.UpdateClock(AdminManager.Now.AddDays(1));
+                break;
+            case EnumTimeUnit.Month:
+                AdminManager.UpdateClock(AdminManager.Now.AddMonths(1));
+                break;
+            case EnumTimeUnit.Year:
+                AdminManager.UpdateClock(AdminManager.Now.AddYears(1));
+                break;
+            default:
+                throw new BlInvalidInputException($"Invalid time unit: {unit}");
+        }
+    }
 
+    public DateTime GetClock()=> AdminManager.Now;
+
+    public Config GetConfig()=> AdminManager.GetConfig();
+
+    public void InitializeDB() => AdminManager.InitializeDB();
+
+    public void ResetDB() => AdminManager.ResetDB();
+
+    public void SetConfig(Config config) => AdminManager.SetConfig(config);
 }
