bf3d1c14064e09b15d7f6a031b1cf7f986d97671
hadas wert <hadaswert1403@gmail.com>
2025-11-30 18:39:41 +0200

    add functions + implementations to Helpers->DeliveryManager

--- BL/Helpers/DeliveryManager.cs
+++ BL/Helpers/DeliveryManager.cs
@@ -1,4 +1,6 @@
-﻿using DalApi;
+﻿using BO;
+using DalApi;
+using DO;
 
 namespace Helpers;
 
@@ -6,15 +8,76 @@ internal static class DeliveryManager
 {
     private static IDal s_dal = Factory.Get; //stage 4
 
-    //GetDelivery ?
-    internal static BO.DeliveryPerOrderInList GetDeliveryPerOrderInList(int orderId)
+    internal static DateTime? CalculateExpectedDeliveryTime(int id)
     {
-        
+        throw new NotImplementedException();
+    }
+
+    internal static EnumOrderStatus CalculateOrderStatus(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static Delivery CreateDemeDelivery(int orderId)
+    {
+        return new DO.Delivery
+        {
+            Id = 0,
+            OrderId = orderId,
+            CourierId = 0,
+            DeliveryMethod = DO.EnumDeliveryMethod.Car, //default value
+            DeliveryStartTime = AdminManager.Now,
+            EndDeliveryStatus = DO.EnumEndDeliveryStatus.Canceled,
+            EndDeliveryTime = AdminManager.Now
+        };
+    }
+
+
+    /// <summary>
+    /// returns the courier ID that delivered the given BO order
+    /// </summary>
+    internal static int? GetCourierIdToBoOrder(BO.Order boOrder)
+    {
+        //get list of deliveries for the given order ID
+        var delList = GetListDeliveryPerOrderInList(boOrder.Id);
+
+        // Find the object where the time difference (boOrder.CreationTime - delivery.DelCreationTime) is minimal
+        // OrderBy-> calculates the time difference for each item and sorts from smallest to largest
+        // FirstOrDefault-> returns the first item in the sorted list (the item with the min time difference)
+        var closestDelivery = delList.OrderBy(delivery => boOrder.CreationTime - delivery.DelCreationTime)
+                                     .FirstOrDefault();
+
+        // Return the CourierId of the found delivery (or null if no delivery was found)
+        return closestDelivery?.CourierId;
+    }
+
+    /// <summary>
+    /// returns a list of DeliveryPerOrderInList for the given BO order ID
+    /// </summary>
+    internal static IEnumerable<BO.DeliveryPerOrderInList> GetListDeliveryPerOrderInList(int orderId)
+    {
+        return s_dal.Delivery.ReadAll().Where(delivery => delivery.OrderId == orderId)
+            .Select(delivery => new BO.DeliveryPerOrderInList()
+            {
+                DeliveryId = delivery.Id,
+                CourierId = delivery.CourierId,
+                CourierName = s_dal.Courier.Read(delivery.CourierId)!.Name,
+                DeliveryMethod = (BO.EnumDeliveryMethod)delivery.DeliveryMethod,
+                DelCreationTime = delivery.DeliveryStartTime,
+                EndDeliveryStatus = (BO.EnumEndDeliveryStatus)delivery.EndDeliveryStatus!,
+                EndDeliveryTime = delivery.EndDeliveryTime
+            }).ToList();
+    }
+
+
+    internal static EnumScheduleStatus GetScheduleStatus(DO.Order doOrder)
+    {
+        throw new NotImplementedException();
     }
 
     internal static TimeSpan GetTotalDeliveryTime(int orderId)
     {
-
+        // look at function OrderManager.BoOrderToBoOrderInList
     }
 
 
