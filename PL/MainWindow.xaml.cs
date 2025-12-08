fa516164310fb0789896c29996a55b665089142a
hadas wert <hadaswert1403@gmail.com>
2025-12-08 23:44:19 +0200

    edit: MainWindow.xaml

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -9,16 +9,85 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PL
+namespace PL;
+
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+
+
+    public MainWindow()
     {
-        public MainWindow()
+
+        InitializeComponent();
+
+    }
+
+    /// <summary>
+    /// property dependency to bind the current time to the UI
+    /// </summary>
+    public DateTime CurrentTime
+    {
+        get { return (DateTime)GetValue(CurrentTimeProperty); }
+        set { SetValue(CurrentTimeProperty, value); }
+    }
+    public static readonly DependencyProperty CurrentTimeProperty =
+        DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow), new PropertyMetadata(DateTime.Now));
+
+
+    private void BtnAddOneMinute_Click(object sender, RoutedEventArgs e)
+    {
+        s_bl.Admin.ForwardClock(BO.EnumTimeUnit.Minute);
+    }
+
+    private void BtnAddOneHour_Click(object sender, RoutedEventArgs e)
+    {
+        s_bl.Admin.ForwardClock(BO.EnumTimeUnit.Hour);
+    }
+
+    private void BtnAddOneDay_Click(object sender, RoutedEventArgs e)
+    {
+        s_bl.Admin.ForwardClock(BO.EnumTimeUnit.Day);
+    }
+
+    private void BtnAddOneMonth_Click(object sender, RoutedEventArgs e)
+    {
+        s_bl.Admin.ForwardClock(BO.EnumTimeUnit.Month);
+    }
+
+    private void BtnAddOneYear_Click(object sender, RoutedEventArgs e)
+    {
+        s_bl.Admin.ForwardClock(BO.EnumTimeUnit.Year);
+    }
+
+
+
+    /// <summary>
+    /// property dependency to bind the Configuration to the UI
+    /// </summary>
+    public BO.Config Configuration
+    {
+        get { return (BO.Config)GetValue(ConfigurationProperty); }
+        set { SetValue(ConfigurationProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Configuration.
+    public static readonly DependencyProperty ConfigurationProperty =
+        DependencyProperty.Register("Configuration", typeof(BO.Config), typeof(MainWindow), new PropertyMetadata(null)); //can't really get null as default
+
+    private void SaveConfigurationCommand(object sender, RoutedEventArgs e)
+    {
+        try
         {
-            InitializeComponent();
+            s_bl.Admin.SetConfig(Configuration);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"Error saving configuration: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
